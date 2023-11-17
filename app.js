767e1a9a740ee879471969dfb92f903e502a6197
Alex Liu <a.liu24@outlook.com>
2023-11-17 11:42:08 -0700

    Added highlight feature

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 // app.js
 // JavaScript code goes here
+
+document.addEventListener('DOMContentLoaded', function () {
+    const categoryItems = document.querySelectorAll(".category-item");
+    highlightGridItems(categoryItems);}
+);
+
+
+function highlightGridItems(gridItems) {
+    let currentIndex = 0;
+
+    function updateHighlight() {
+        gridItems.forEach(item => item.classList.remove("highlight"));
+        gridItems[currentIndex].classList.add("highlight");
+        currentIndex = (currentIndex + 1) % gridItems.length;
+    }
+
+    updateHighlight();
+    setInterval(updateHighlight, 3000);
+}
+
