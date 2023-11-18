cef73aa038eb37f7fdf257e149096968373d1d9e
kstark1 <kstark@ualberta.ca>
2023-11-17 18:02:03 -0700

    merged with alex1 branch

--- Public/app.js
+++ Public/app.js
@@ -1,11 +1,15 @@
 // app.js
 // JavaScript code goes here
+import { RadioBrowserApi, StationSearchType } from 'https://cdn.skypack.dev/radio-browser-api';
+
 let categorySection, radioSection, categoryItems, radioItems, settingsSection, settingsItems;
 let sectionList = {CATEGORY: 'category', RADIO: 'radio', SETTINGS: 'settings'};
 let currentSectionDisplayed;
 let currentIndex = 0;
 let previousTabDisplayed;
 let settingsList = {VOLUME: 'volume', HTIME : 'htime', NUMRADIO: 'numradio', NUMCAT: 'numcat'};
+
+const radio_api = new RadioBrowserApi('My Radio App')
 
 document.addEventListener('DOMContentLoaded', function () {
     categoryItems = document.querySelectorAll(".category-item");
@@ -29,8 +33,7 @@ document.addEventListener('keydown', function(e) {
     const keyPressed = e.key;
     if (keyPressed === "s"){
         if (currentSectionDisplayed === sectionList.CATEGORY) {
-            getRadioStationByCategory();
-            toggleTabVisibility();
+            handleSKeyPressed();
         }
     }
 
@@ -39,6 +42,11 @@ document.addEventListener('keydown', function(e) {
         
     }
 })
+
+async function handleSKeyPressed() {
+    await getRadioStationByCategory();
+    toggleTabVisibility();
+}
 
 function highlightGridItems(gridItems) {
     currentIndex = 0;
@@ -103,16 +111,19 @@ async function getRadioStationByCategory(){
     let categoryName = categoryItem.querySelector("p").innerText.toLowerCase();
 
     try {
-        const response = await fetch(`http://localhost:3000/api/radio?category=${encodeURIComponent(categoryName)}`);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
+        const stations = await radio_api.searchStations({
+          tagList: [categoryName],
+          limit: 100,
+          offset: 0,
+        });
 
-        const data = await response.json();
-        console.log(data); 
-    } catch (error) {
+        console.log("Station Retrieved Successully");
+    
+        return stations;
+      } catch (error) {
         console.error('Error:', error);
-    }
+        throw new Error('Error fetching radio stations');
+      }
 }
 
 async function getSettings(){
