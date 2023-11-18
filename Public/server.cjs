00e26287ecb246de464ab095d0e900e06495448f
Igauk <igauk@ualberta.ca>
2023-11-18 11:03:19 -0700

    Host assets statically in express server at /Assets

--- Public/server.cjs
+++ Public/server.cjs
@@ -3,9 +3,10 @@ const path = require('path');
 const app = express();
 const port = 4000;
 
-console.log(path.join(__dirname, 'Public'))
+const assetsPath = path.join(path.dirname(__dirname), 'Assets');
 
-app.use(express.static(path.join(__dirname)));
+app.use('/Assets', express.static(assetsPath));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -13,4 +14,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Static files are served from ${assetsPath}`);
 });
